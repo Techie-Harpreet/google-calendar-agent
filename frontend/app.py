d427f2c1325619399ddde70e904c1bdeded2ca87
Techie Harpreet <93361317+Techie-Harpreet@users.noreply.github.com>
2025-07-02 12:18:15 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -21,7 +21,9 @@ st.caption("Your friendly AI agent for booking appointments")
 # The backend API URL
 # When running locally, both are on your machine.
 # When deployed, this will be the URL of your deployed backend.
-BACKEND_URL = "http://127.0.0.1:8000/chat" 
+# BACKEND_URL = "http://127.0.0.1:8000/chat" 
+BACKEND_URL = "https://google-calendar-agent-jzlh.onrender.com" 
+
 
 # Initialize session_id if it doesn't exist
 if 'session_id' not in st.session_state:
